aeba463d1b3d7ee6c34b2e394ebb6c7424562781
Pablo Sarti <Paulsarti7@gmail.com>
2023-10-26 16:05:44 -0400

    Delete my_new_env directory

--- my_new_env/lib/xsltConf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the xslt library
-#
-XSLT_LIBDIR="-L/Users/pablosarti/Personal Portfolio/RSS Feed Reader/my_new_env/lib"
-XSLT_LIBS="-lxslt -L/Users/pablosarti/Personal Portfolio/RSS Feed Reader/my_new_env/lib -lxml2 "
-XSLT_PRIVATE_LIBS=""
-XSLT_INCLUDEDIR="-I/Users/pablosarti/Personal Portfolio/RSS Feed Reader/my_new_env/include"
-MODULE_VERSION="xslt-1.1.37"
